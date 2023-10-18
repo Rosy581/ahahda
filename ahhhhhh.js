6497effa06f340caea6f4c88e11a7e9a36186203
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-10-17 20:15:14 -0400

    Update ahhhhhh.js
    updated the asciifii function :3

--- ahhhhhh.js
+++ ahhhhhh.js
@@ -2,33 +2,42 @@
 Things to save
 Level
 Money
-Stats
+Stats 
 Name
 game states
 area
 */
-var lvl = "1";
-    var moni;
-    var str;
-    var hp;
-    var mgc;
-    var name;
-    var gs;
-    var loc;
+var lvl = "21";
+var moni = 125;
+var str;
+var hp;
+var mgc;
+var ris;
+var name;
+var gs;
+var loc;
 //ahhhhhh
-    function asciiIfi(asc){
-        var Temp = asc;
-        var leng = (asc.length);
+function asciiIfi(asc) {
+    if (typeof asc === "number") {
+        asc = asc.toString();
+    }
+    var leng = asc.length;
+    var final = "";
+
+    for (let i = 0; i < leng; i++) {
+        console.log(asc.charCodeAt(i));
+        final += asc.charCodeAt(i);
+    }
+    return final;
 }
-function load(saveString){
+function load(saveString) {
     const Load1 = saveString.split("-")
     console.log(String.fromCharCode(Load1[0]))
-    }  
+}
+function save() {
+    var saveLVL = asciiIfi(lvl);
+    var saveMon = asciiIfi(moni);
+    let saveString = saveLVL + "-" + saveMon + "-";
+}
 
-    function save(){
-        let saveString;
-        var saveLVL= String.fromCharCode(lvl)
-        console.log(saveLVL)
-        }
-    load('49-49')
-    save()
+save()
