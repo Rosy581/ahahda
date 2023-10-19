6a6830e306549bd51fa945a7df9ecbce795f66e7
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-10-19 14:47:19 -0400

    Update ahhhhhh.js
    AHHH
    NO

--- ahhhhhh.js
+++ ahhhhhh.js
@@ -7,55 +7,63 @@ Name
 game states
 area
 */
-var lvl = "21";//level uwu
-var moni = 125;//dabloons
-var str; //strength for mellee weapons
-var hp; // current healtjj
-var mh; //max health
-var mgc; // magic for like magic yk
-var ris; //resistence?
-var named="";//cant change to name js spaghetti
-var gs; //gamestate
-var loc; //location
-//ahhhhhh
-function asciiIfi(asc) {
-	if (typeof asc === "number") {
-		asc = asc.toString();
-	} else if (typeof asc === "undefined"||typeof asc === "boolean"){
-		asc = 0;
-	}
-	var leng = asc.length;
-	let final = "";
+var lvl = "21"; // level uwu
+var moni = 125; // dabloons
+var str; // strength for melee weapons
+var hp; // current health
+var mh; // max health
+var mgc; // magic for like magic, you know
+var ris; // resistance?
+var named = "Greg"; // can't change to name, JS spaghetti
+var gs; // game state
+var loc; // location
 
-	for (let i = 0; i < leng; i++) {
-		console.log(asc.charCodeAt(i));
-		final += asc.charCodeAt(i);
-	}
+function asciiIfi(asc) {
+    if (typeof asc === "number") {
+        asc = asc.toString();
+    } else if (typeof asc === "undefined" || typeof asc === "boolean") {
+        asc = "0";
+    }
+    var leng = asc.length;
+    let final = "";
+
+    for (let i = 0; i < leng; i++) {
+        console.log(asc.charCodeAt(i));
+        final += asc.charCodeAt(i);
+    }
 	return final;
 }
-
+/*
+Fix load function
+rn it splits to an array maybe use that to get the substrings?
+figure out a way to sperate each charcode
+*/
 function load(saveString) {
-	const Load1 = saveString.split("-")
-	
-
-}
-function nameSav() {
-	const saveNam = [];
-	let len = named.length;
-	for (let i = 0; i < len; i++) {
-        saveNam.push(named.fromCharCode[i])
+    const Load1 = saveString.split("-");
+	for (let i=0; i<saveString.length();i++){
+		let h=2
+		console.log(String.fromCharCode(saveString.substring()))
 	}
-	var savName=saveNam.join(', ')
-	return savName
-}
- ''
-function save() {
-	var saveLevel = asciiIfi(lvl);
-	var saveMoney = asciiIfi(moni);
-	var saveName = nameSav();
-	var saveLocation = asciiIfi(loc)
-	var saveResistence = asciiIfi(ris)
-	let saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" +;
 }
 
-save()
+function nameSav() {
+    const saveNam = [];
+    let len = named.length;
+    for (let i = 0; i < len; i++) {
+        saveNam.push(named.charAt(i));
+    }
+    var savName = saveNam.join(', ');
+    return savName;
+}
+
+function save() {
+    var saveLevel = asciiIfi(lvl);
+    var saveMoney = asciiIfi(moni);
+    var saveName = nameSav();
+    var saveLocation = asciiIfi(loc);
+    var saveResistence = asciiIfi(ris);
+    var saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" + saveResistence;
+    return saveString;
+}
+
+save();
