0b8fc6326418c4f5d0a22f9ed33c65f37a077210
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-10-20 19:55:41 -0400

    Update ahhhhhh.js
    :3

--- ahhhhhh.js
+++ ahhhhhh.js
@@ -1,12 +1,3 @@
-/*
-Things to save
-Level
-Money
-Stats 
-Name
-game states
-area
-*/
 var lvl = "21"; // level uwu
 var moni = 125; // dabloons
 var str; // strength for melee weapons
@@ -19,18 +10,16 @@ var gs; // game state
 var loc; // location
 
 function asciiIfi(asc) {
-    if (typeof asc === "number") {
-        asc = asc.toString();
-    } else if (typeof asc === "undefined" || typeof asc === "boolean") {
-        asc = "0";
-    }
-    var leng = asc.length;
-    let final = "";
-
-    for (let i = 0; i < leng; i++) {
-        console.log(asc.charCodeAt(i));
-        final += asc.charCodeAt(i);
-    }
+	if (typeof asc === "number") {
+		asc = asc.toString();
+	} else if (typeof asc === "undefined" || typeof asc === "boolean") {
+		asc = "0";
+	}
+	var leng = asc.length;
+	let final = "";
+	for (let i = 0; i < leng; i++) {
+		final += asc.charCodeAt(i);
+	}
 	return final;
 }
 /*
@@ -38,32 +27,31 @@ Fix load function
 rn it splits to an array maybe use that to get the substrings?
 figure out a way to sperate each charcode
 */
+     function load(saveString) {
+        const Load1 = saveString.split("-");
+        for (let i = 0; i < saveString.length(); i++) {
+            let substring = Load1[i]
+            for	(let h = 0; h<){
+                console.log(String.fromCharCode(subtring.fromCharCode(h,i)))
+            }
+        }
+    }
 function load(saveString) {
-    const Load1 = saveString.split("-");
-	for (let i=0; i<saveString.length();i++){
-		let h=2
-		console.log(String.fromCharCode(saveString.substring()))
+	const Load1 = saveString.split("-");
+	for (let i = 0; i < saveString.length; i++) {
+        let substring = Load1[i]
+	    for	(let h = 0; h<substring.length;h+=2){
+		    console.log(String.fromCharCode(substring.charAt(h)))
+        }
 	}
 }
-
-function nameSav() {
-    const saveNam = [];
-    let len = named.length;
-    for (let i = 0; i < len; i++) {
-        saveNam.push(named.charAt(i));
-    }
-    var savName = saveNam.join(', ');
-    return savName;
-}
-
 function save() {
-    var saveLevel = asciiIfi(lvl);
-    var saveMoney = asciiIfi(moni);
-    var saveName = nameSav();
-    var saveLocation = asciiIfi(loc);
-    var saveResistence = asciiIfi(ris);
-    var saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" + saveResistence;
-    return saveString;
+	var saveLevel = asciiIfi(lvl);
+	var saveMoney = asciiIfi(moni);
+	var saveName = nameSav();
+	var saveLocation = asciiIfi(loc);
+	var saveResistence = asciiIfi(ris);
+	var saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" + saveResistence;
+	return saveString;
 }
-
 save();
