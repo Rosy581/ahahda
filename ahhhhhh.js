d54bfc2e9c86979e367fc8a7b163297f03a0ffa3
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-10-26 14:40:20 -0400

    :(
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH NO WHY OH FUCK

--- ahhhhhh.js
+++ ahhhhhh.js
@@ -1,67 +1,70 @@
-	var lvl = "21"; // level uwu
-	var moni = 125; // dabloons
-	var str; // strength for melee weapons
-	var hp; // current health
-	var mh; // max health
-	var mgc; // magic for like magic, you know
-	var ris; // resistance?
-	var named = "Greg"; // can't change to name, JS spaghetti
-	var gs; // game state
-	var loc; // location
-	function nameSav() {
-		const saveNam = [];
-		let len = named.length;
-		for (let i = 0, h = 1; i < len; i++,h++) {
-			saveNam.push(String.fromCharCode(named.substring(i,h)))
-		}
-		var savName = saveNam.join(', ')
-		return savName
-	}
-	function asciiIfi(asc) {
-		if (typeof asc === "number") {
-			ascii = asc.toString();
-			var leng = ascii.length;
-			let final = "";
-			for (let i = 0; i < leng; i++) {
-				final += ascii.charCodeAt(i);
-				return final; 
-			}
-		} else if (typeof asc === "string") {
-			var asci = parseInt(asc)
-			if (isNaN(asci)) {
-				var x = Error("nuh uh")
-				console.log(x)
-			} else {
-				var leng = asc.length;
-				let final = "";
-				for (let i = 0; i < leng; i++) {
-					final += asc.charCodeAt(i);
-				}
-				return final;
-			}
-		} else if (!typeof asc === "number" || !typeof asc === "string") {
-			asc = "0";
-		}
-	}
-	/*
-	1.Make second function to do each segment
-	2. assign each segment to its corrisponding varible 
-	*/
-	function load(saveString) {
-		const Load1 = saveString.split("-");
-		const Load2 = Load1[x].toString();
-		for (let i = 0, h=2; i < Load2.length; i+=2,h+=2) {
-			console.log(String.fromCharCode(Load2.substring(h,i)))   
-		}
-	}
+var lvl = "21"; // level uwu
+var moni = 125; // dabloons
+var str; // strength for melee weapons
+var hp; // current health
+var mh; // max health
+var mgc; // magic for like magic, you know
+var ris; // resistance?
+var named = "GREG"; // can't change to name, JS spaghetti
+var gs; // game state
+var loc; // location
+function removecoma(rem) {
+    remo = rem.split(",");
+    remov = remo.join("");
+    return remov;
+}
 
-	function save() {
-		var saveLevel = asciiIfi(lvl);
-		var saveMoney = asciiIfi(moni);
-		var saveName = nameSav();
-		var saveLocation = asciiIfi(loc);
-		var saveResistence = asciiIfi(ris);
-		var saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" + saveResistence;
-		return saveString;
-	}
-	save();
+function varSav(sava) {
+    const saveVar = [];
+    let a = parseInt(sava)
+    var vared = sava
+	var sav = sava.toString();
+    if (typeof vared === "string" && isNaN(a)) {
+        vared = sava.toUpperCase();
+    } else if (typeof sava === "number") {
+        vared = sava.toString();
+    }
+    let len = sava.length;
+    for (let i = 0; i < len; i++) {
+        saveVar.push(vared.charCodeAt(i))
+    }
+    var varSavi = saveVar.join('')
+    return varSavi
+}
+/*
+1.Make second function to do each segment
+2. assign each segment to its corrisponding varible 
+*/
+function load(saveString) {
+    const fin = [
+        [],
+        [],
+        []
+    ]
+    const Load1 = saveString.split("-");
+    const Load2 = Load1.length;
+    for (let x = 0; x < Load2; x++) {
+        const Load3 = Load1[x].toString();
+        for (let i = 0, h = 2; i < Load3.length; i += 2, h += 2) {
+            fin[x].push(String.fromCharCode(Load3.substring(h, i)))
+        }
+    }
+    Load3 = fin.length;
+    for (let j = 0; j < Load3; j++) {
+        console.log(fin)
+        const fina = fin.join("")
+        const final = removecoma(fina)
+        return final
+    }
+}
+
+function save() {
+    var saveLevel = varSav(lvl);
+    var saveMoney = varSav(moni);
+    var saveName = varSav(named);
+    var saveLocation = varSav(loc);
+    var saveResistence = varSav(ris);
+    var saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" + saveResistence;
+    return saveString;
+}
+save();
