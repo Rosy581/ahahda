3431d8d7705045bbbb4bd8cd787e6ce37c8da8d3
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-10-27 11:52:43 -0400

    upperCase(PAIN)

--- ahhhhhh.js
+++ ahhhhhh.js
@@ -4,7 +4,7 @@ var str; // strength for melee weapons
 var hp; // current health
 var mh; // max health
 var mgc; // magic for like magic, you know
-var ris; // resistance?
+var ris = []; // resistance?
 var named = "GREG"; // can't change to name, JS spaghetti
 var gs; // game state
 var loc; // location
@@ -20,34 +20,63 @@ function varSav(sava) {
     var vared = sava
     if (typeof vared === "string" && isNaN(a)) {
         vared = sava.toUpperCase();
-		let len = vared.length;
-		for (let i = 0; i < len; i++) {
-			saveVar.push(vared.charCodeAt(i))
-		}
-		var varSavi = saveVar.join('')
-		return varSavi
-	} else if (typeof sava === "number") {
+        let len = vared.length;
+        for (let i = 0; i < len; i++) {
+            saveVar.push(vared.charCodeAt(i))
+        }
+        var varSavi = saveVar.join('')
+        return varSavi
+    } else if (typeof sava === "number") {
         vared = sava.toString();
-		let len = vared.length;
-		for (let i = 0; i < len; i++) {
-			saveVar.push(vared.charCodeAt(i))
-		}
-		var varSavi = saveVar.join('')
-		return varSavi
-	}else{function kys (num){for(let i = 0; i<num;i++){console.log("kys")}}}
+        let len = vared.length;
+        for (let i = 0; i < len; i++) {
+            saveVar.push(vared.charCodeAt(i))
+        }
+        var varSavi = saveVar.join('')
+        return varSavi
+    } else {
+        function kys(num) {
+            for (let i = 0; i < num; i++) {
+                console.log("kys")
+            }
+        }
+    }
 
 }
 
 /*
 1.Make second function to do each segment
 2. assign each segment to its corrisponding varible 
+do this by using object things
+ex.
+function isLetter(){return test = "letter"}
+function isNumber(){return test = "number"}
+var test = {
+    "1":isNumber(),
+    "a":isLetter(),
+}
+test["1"]
+returns number
+test["a"]
+returns letter
+use this to take the value of x to assign varibles
+for their array pos also can
+
+might want to scramble the saveString to make it harder to crack.
 */
+//funcy town
+function pushLVL(nLVL){
+    lvl = nLVL
+}
+function pushVar(varTBP){
+    var type = prompt("Input var to be pushed")
+    var vats = {
+        "lvl":pushLVL(varTBP)
+    }
+
+}
 function load(saveString) {
-    const fin = [
-        [],
-        [],
-        []
-    ]
+    const fin = []
     const Load1 = saveString.split("-");
     const Load2 = Load1.length;
     for (let x = 0; x < Load2; x++) {
@@ -74,4 +103,22 @@ function save() {
     var saveString = saveLevel + "-" + saveMoney + "-" + saveName + "-" + saveLocation + "-" + saveResistence;
     return saveString;
 }
-save();
+/*
+mechanics functions now
+*/
+function lvlUP() {
+    lvl = parseInt(lvl)
+    lvl = lvl += 1
+    console.log("LEVELUP!!")
+    return lvl
+}
+
+function moneyUp(add) {
+    add = parseInt(add)
+    moni = moni += add
+    return moni
+}
+
+function rizzUp(rlvl, typ) {
+
+}
---
+++ toUpperCase but mine.js
@@ -0,0 +1,50 @@
+function upA(){
+
+}
+function upB(){}
+function upC(){}
+function upD(){}
+function upE(){}
+function upF(){}
+function upG(){}
+function upH(){}
+function upI(){}
+function upJ(){}
+function upK(){}
+function upL(){}
+function upM(){}
+function upN(){}
+function upO(){}
+function upP(){}
+function upQ(){}
+function upR(){}
+function upS(){}
+function upT(){}
+function upU(){}
+function upV(){}
+function upW(){}
+function upX(){}
+function upY(){}
+function upZ(){}
+var type="";
+var killME={
+    "a": upA(),
+    "b": upB(),
+    "c": upC(),
+    "d": upD(),
+    "e": upE(),
+    "f": upF(),
+    "g": upG(),
+    "h": upH()
+}
+
+
+function toUpperCase(string){
+    var final=[]
+    for(let i = 0; i<string.length;i++)
+    {
+       string.charAt(i));
+    }
+    final=final.join("")
+    return final
+}
