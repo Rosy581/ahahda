14919f067b5b263ede36189e7bea4dc9d5a47672
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-12-14 13:33:33 -0500

    made teams

--- jepordy.ts
+++ jepordy.ts
@@ -1,45 +1,47 @@
-class questions {
-    question: string
-    value: number
-    constructor(question, answer, value) {
-        this.question = question
-        this.answer = answer
-        this.value = value
-        this.answered = false
-        this.timesUp = new Audio('https://www.myinstants.com/media/sounds/times-up.mp3')
+class team{
+    classId:number
+    name:stringm
+    constructor(name){
+        this._name = name
     }
-    get answer() {
+}
+class questions {
+    _question:string
+    _value:number
+    _answer:string;
+    _answered = false
+    _timesUp = new Audio('https://www.myinstants.com/media/sounds/times-up.mp3')
+    constructor(question:string,answer:string,value:number){
+        this._question = question
+        this._answer = answer
+        this._value = value
+    }
+    get answer(){
         return this.answer
     }
-    get question() {
+    get question(){
         return this.question
     }
-    get value() {
+    get value(){
         return this.value
     }
-}
-function genQues(question) {
-    createElement()// put code for question
-}
+ 
+
 //figure out how to set up so when you press during the 30 seconds it will stop the timer
-async function timer(active: boolean) {
-    if (active === true) {
-        return new Promise((resolve, reject) => {
-            resolve(timesUp.play())
-        })
-    }
+function add(team:number ,amount:number){
+    team.points+=amount
+    _answered = true
 }
-function add(team, amount) {
-    team += amount
-    this.answered = true
+function subtract(team,amount){
+    team-=amount
 }
-function subtract(team, amount) {
-    team -= amount
 }
-function qState() {
-    //update state of question from 
-}
-function main() {
+function main(){
     //return to homescreen and remove answered questions
 
+}
+const test = new questions()
+function genQues(question:object){
+    // put code for question
+    
 }
