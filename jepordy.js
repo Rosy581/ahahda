89d284a22ff3aeddec031d278ddd236b4500c21d
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-12-14 08:14:06 -0500

    jeep

---
+++ jepordy.js
@@ -0,0 +1,31 @@
+ class questions {
+    constructor(question,answer){
+        this.question = question
+        this.answer = answer
+    }
+    get answer(){
+        return this.answer
+    }
+    get question(){
+        return this.question
+    }
+    sumbit(ans){
+        if(ans === answer)
+    }
+ }
+function genQues(question){
+    createElement()// put code for question
+}
+function add(team,amm){
+    team+=amm
+}
+function subtract(team,amm){
+    team-=amm
+}
+function qState(){
+    //update state of question from 
+}
+function main(){
+    //return to homescreen and remove answered questions
+
+}
