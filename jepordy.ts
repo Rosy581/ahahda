b5514bfdf87098fe3d8422ce3f22dd5cc51a7bea
BoomerJr <65262966+BoomerJr@users.noreply.github.com>
2023-12-14 13:24:26 -0500

    uhhhh

--- jepordy.ts
+++ jepordy.ts
@@ -1,45 +1,45 @@
- class questions {
-    question:string
-    value:number
-    constructor(question,answer,value){
+class questions {
+    question: string
+    value: number
+    constructor(question, answer, value) {
         this.question = question
         this.answer = answer
         this.value = value
         this.answered = false
         this.timesUp = new Audio('https://www.myinstants.com/media/sounds/times-up.mp3')
     }
-    get answer(){
+    get answer() {
         return this.answer
     }
-    get question(){
+    get question() {
         return this.question
     }
-    get value(){
+    get value() {
         return this.value
     }
- }
-function genQues(question){
+}
+function genQues(question) {
     createElement()// put code for question
 }
 //figure out how to set up so when you press during the 30 seconds it will stop the timer
-async function timer(active:boolean) {
-    if(active === true){
-        return new Promise((resolve,reject) =>{
+async function timer(active: boolean) {
+    if (active === true) {
+        return new Promise((resolve, reject) => {
             resolve(timesUp.play())
         })
     }
 }
-function add(team,amount){
-    team+=amount
+function add(team, amount) {
+    team += amount
     this.answered = true
 }
-function subtract(team,amount){
-    team-=amount
+function subtract(team, amount) {
+    team -= amount
 }
-function qState(){
+function qState() {
     //update state of question from 
 }
-function main(){
+function main() {
     //return to homescreen and remove answered questions
 
 }
